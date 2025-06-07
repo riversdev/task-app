f250829f5d1ab8ff18c5ff59803260b7894aa26b
Alejandro Ríos <alejandroriosdev@gmail.com>
2025-06-07 16:20:24 -0600

    feat: app theme

--- apps/task-app/src/main.tsx
+++ apps/task-app/src/main.tsx
@@ -2,6 +2,10 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { App } from '@/App'
 
+import '@/styles/appTheme.css'
+import 'bootstrap/dist/js/bootstrap.bundle'
+import '@/styles/app.css'
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
